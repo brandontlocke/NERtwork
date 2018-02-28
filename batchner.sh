92a6daad1fe9f9b25f2c4b64e1776c5eda85b6c3
Brandon Locke <brandontlocke@gmail.com>
2018-02-28 15:48:42 -0500

    Create batchner.sh

---
+++ batchner.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+echo "entity,doc,type,count" > entities.csv
+
+for file in *.txt
+
+do
+
+nertext=$(../stanford-ner-2017-06-09/ner.sh $file)
+
+echo $nertext | egrep -o "(([[:alnum:]]|\.)+/ORGANIZATION([[:space:]]|$))+" | sed 's/\/ORGANIZATION//g' | sort | uniq -c | awk -v name=${file##*/} '{printf name ","; for (i = 2; i < NF; i++) printf $i " "; printf $NF; printf "," "organization" ","; printf $1;  print ""}' >> entities.csv
+
+echo $nertext | egrep -o "(([[:alpha:]]|\.)*/PERSON([[:space:]]|$))+" | sed 's/\/PERSON//g' | sort | uniq -c | awk -v name=${file##*/} '{printf name ","; for (i = 2; i < NF; i++) printf $i " "; printf $NF; printf "," "person" ","; printf $1;  print ""}' >> entities.csv
+
+echo $nertext | egrep -o "(([[:alnum:]]|\.)*/LOCATION[[:space:]](,[[:space:]])?)+" | sed 's/\/LOCATION//g' | sort | uniq -c | awk -v name=${file##*/} '{printf name ","; for (i = 2; i < NF; i++) printf $i " "; printf $NF; printf "," "location" ","; printf $1;  print ""}' >> entities.csv
+
+done

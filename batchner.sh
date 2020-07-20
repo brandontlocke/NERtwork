0dbcd020f8d53b3aeb908754840f4d328c50f9c8
Brandon Locke <brandontlocke@gmail.com>
2020-07-20 10:25:58 -0700

    Update batchner.sh

--- batchner.sh
+++ batchner.sh
@@ -5,8 +5,8 @@ do
 ############################
 #If you're using Windows, delete the # from the start of line 8 and add a # to the start of line 9
 ############################
-#nertext=$(java -mx600m -cp ../stanford-ner-4.0.0/stanford-ner.jar edu.stanford.nlp.ie.crf.CRFClassifier -loadClassifier ../stanford-ner-4.0.0/classifiers/english.all.3class.distsim.crf.ser.gz -textFile $file)
-nertext=$(../stanford-ner-4.0.0/ner.sh $file)
+#nertext=$(java -mx600m -cp ../stanford-ner/stanford-ner.jar edu.stanford.nlp.ie.crf.CRFClassifier -loadClassifier ../stanford-ner/classifiers/english.all.3class.distsim.crf.ser.gz -textFile $file)
+nertext=$(../stanford-ner/ner.sh $file)
 
 echo $nertext | egrep -o "(([[:alnum:]]|\.)+/ORGANIZATION([[:space:]]|$))+" | sed 's/\/ORGANIZATION//g' | sort | uniq -c | awk -v name=${file%%.*} '{printf name ","; for (i = 2; i < NF; i++) printf $i " "; printf $NF; printf "," "organization" ","; printf $1;  print ""}' >> entities.csv
 echo $nertext | egrep -o "(([[:alpha:]]|\.)*/PERSON([[:space:]]|$))+" | sed 's/\/PERSON//g' | sort | uniq -c | awk -v name=${file%%.*} '{printf name ","; for (i = 2; i < NF; i++) printf $i " "; printf $NF; printf "," "person" ","; printf $1;  print ""}' >> entities.csv
